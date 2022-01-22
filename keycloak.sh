59d5e4867571bf7d148c73c52aac6b490f398554
Kasullian <tomkasull@gmail.com>
2022-01-22 05:13:38 +0000

    16.1.0 xml, updated env var structure

--- keycloak.sh
+++ keycloak.sh
@@ -1,5 +1,4 @@
 export DOMAIN_SUBDOMAIN=auth.domain.com # for example mykeycloak.com or keycloak.mydomain.com
-export KEYCLOAK_SSL_ALIAS=domain # the alias for your SSL certificate which can be anything but avoid weird characters
+export KEYCLOAK_SSL_ALIAS=alias # the alias for your SSL certificate which can be anything but avoid weird characters
 export KEYCLOAK_SSL_PASSWORD=password123 # the password for your SSL certificate, again avoid weird characters
-export KEYSTORE_PASSWORD=password123 # this will be the password to access the Java keystore where your certificate will live, should be the same as your SSL_PASSWORD
-export KEYCLOAK_PASSWORD=password
+export KEYCLOAK_PASSWORD=password # password for initial admin account
